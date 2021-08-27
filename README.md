99509b4b3eaa828192af6125447242d85783ceb9
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-26 19:03:54 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Socket.io 
+
+##This is a small chat app I built to understand web sockets. 
--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,50 @@
 
+function Calculator() {
+  this.dataStore = []
+  this.add = add
+  this.clearData = clearData
+  // this.mul = mul
+  // this.divide = divide
+  // this.subtract = subtract
+  // this.addition = addition
+}
+
+function add(e) {
+  this.dataStore.push(e)
+}
+
+function clearData() {
+  this.dataStore.length = 0
+}
+
+let calculator = new Calculator()
+
+function setRes(c) {
+  document.getElementById('res').innerHTML = c.dataStore.join('')
+}
+
+document.getElementById('btn0').addEventListener('click', function (e) {
+  calculator.add(e.target.innerHTML)
+  setRes(calculator)
+})
+
+document.getElementById('btn1').addEventListener('click', function (e) {
+  calculator.add(e.target.innerHTML)
+  setRes(calculator)
+})
+// Addition sign
+document.getElementById('btnSum').addEventListener('click', function (e) {
+  calculator.add(e.target.innerHTML)
+  setRes(calculator)
+})
+
+// Removes all items from datastore
+document.getElementById('btnClr').addEventListener('click', function () {
+  calculator.clearData()
+  setRes(calculator)
+})
+
+
+
+
 
